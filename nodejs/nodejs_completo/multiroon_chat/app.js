eda23482cc3c6c1556c25a725d0edb6004f84207
Moisés Salvador Escurra Aguilar <moisesmagno@gmail.com>
2017-03-02 00:40:38 -0300

    Comecado a estudar websocket e socket.io

--- nodejs/nodejs_completo/multiroon_chat/app.js
+++ nodejs/nodejs_completo/multiroon_chat/app.js
@@ -2,6 +2,21 @@
 var app = require('./config/server');
 
 //Parametrizar a porta de escuta
-app.listen(8080,function(){
+var server = app.listen(8080,function(){
 	console.log('Servidor online :)');
 });
+
+//Fala para o socket.io ouvir pela porta 8080 tmb.
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+//Cria conexão via websocket
+io.on('connection', function(socket){
+	console.log('O usuário se conectou!');
+
+	socket.on('disconnect', function(){
+		console.log('O usuário se desconectado!');
+	});
+});
+
--- nodejs/nodejs_completo/multiroon_chat/app/controllers/ChatController.js
+++ nodejs/nodejs_completo/multiroon_chat/app/controllers/ChatController.js
@@ -3,7 +3,7 @@ module.exports.iniciarChat = function(application, req, res){
 	var dadosForm = req.body;
 
 	req.assert('apelido', 'O nome ou aplido é obrigatório!').notEmpty();
-	req.assert('apelido', 'O nome ou apelido tem que term entre 3 ou 10 caracteres!').len(3, 10);
+	req.assert('apelido', 'O nome ou apelido tem que term entre 3 ou 10 caracteres!').len(3, 20);
 
 	var erros = req.validationErrors();
 
@@ -12,5 +12,14 @@ module.exports.iniciarChat = function(application, req, res){
 		return;
 	}
 
+	application.get('io').emit(
+		'msgCliente',
+		{
+			apelido: dadosForm.apelido,
+			msg: ' Acabou de entrar no chat:)'
+		}
+		);
+
 	res.render('chat');
 }
+
