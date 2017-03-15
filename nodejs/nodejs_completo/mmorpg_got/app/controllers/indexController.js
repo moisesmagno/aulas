f1943be4dba946f11b46776e37e19ec337e87731
Moisés Salvador Escurra Aguilar <moisesmagno@gmail.com>
2017-03-14 23:34:34 -0300

    Instaldo e configurado o session no node.js

--- nodejs/nodejs_completo/mmorpg_got/app/controllers/indexController.js
+++ nodejs/nodejs_completo/mmorpg_got/app/controllers/indexController.js
@@ -1,3 +1,21 @@
 module.exports.index = function(application, req, res){
-	res.render('index');
+	res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function(application, req, res){
+
+	var dadosForm = req.body;
+
+	req.assert('usuario', 'Informar o usuário é obrigatório!').notEmpty();
+	req.assert('senha', 'A senha é obrigatório!').notEmpty();
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render('index', {validacao: erros, dadosForm: dadosForm});
+		return;
+	}
+
+	res.send('Está tudo ok!');
+
 }
