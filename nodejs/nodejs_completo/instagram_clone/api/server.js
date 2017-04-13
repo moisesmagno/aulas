4fbbe11accb4521a4d255c4557446c6a96296b48
Moisés Salvador Escurra Aguilar <moisesmagno@gmail.com>
2017-04-12 23:56:50 -0300

    Criado parcialmente as apis do curso

--- nodejs/nodejs_completo/instagram_clone/api/server.js
+++ nodejs/nodejs_completo/instagram_clone/api/server.js
@@ -9,8 +9,50 @@ app.use(bodyParser.json());
 
 app.listen(8080);
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
 console.log('O servidor HTTP está escutando na porta:' + 8080);
 
 app.get('/', function(req, res){
     res.send({msg: 'Olá :)'});
+});
+
+//POST
+app.post('/api', function(req, res){
+    var dados = req.body;
+
+    db.open(function(error, mongoclient){
+        mongoclient.collection('postagens', function(error, collection){
+            collection.insert(dados, function(error, result){
+                if(error){
+                    res.json(error);
+                }else{
+                    res.json(result);
+                }
+
+                mongoclient.close();
+
+            });
+        });
+    });
+});
+
+//GET
+app.get('/api', function(req, res){
+    db.open(function(error, mongoclient){
+        mongoclient.collection('postagens', function(error, collection){
+            collection.find().toArray(function(error, result){
+                if(error){
+                    res.json(error);
+                }else{
+                    res.json(result);
+                }
+                mongoclient.close();
+            });
+        });
+    });
 });
