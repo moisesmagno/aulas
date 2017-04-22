0ee91534cd5a49c4e17f8903226cc479b2f71f24
Moisés Salvador Escurra Aguilar <moisesmagno@gmail.com>
2017-04-22 14:15:17 -0300

    Criado mais metodos http para a api do projeto instragram clone

--- nodejs/nodejs_completo/instagram_clone/api/server.js
+++ nodejs/nodejs_completo/instagram_clone/api/server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    mongodb = require('mongodb');
+    mongodb = require('mongodb'),
+    objectId = require('mongodb').ObjectId;
 
 var app = express();
 
@@ -41,11 +42,29 @@ app.post('/api', function(req, res){
     });
 });
 
-//GET
-app.get('/api', function(req, res){
+
+// GET
+app.get('/api', function(req,res){
     db.open(function(error, mongoclient){
         mongoclient.collection('postagens', function(error, collection){
             collection.find().toArray(function(error, result){
+                if(error){
+                    res.json(error);
+                }else {
+                    res.json(result);
+                }
+
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+//GET by Id (ready)
+app.get('/api/:id', function(req, res){
+    db.open(function(error, mongoclient){
+        mongoclient.collection('postagens', function(error, collection){
+            collection.find(objectId(req.params.id)).toArray(function(error, result){
                 if(error){
                     res.json(error);
                 }else{
@@ -53,6 +72,65 @@ app.get('/api', function(req, res){
                 }
                 mongoclient.close();
             });
+        });
+    });
+});
+
+//GET by Id and titulo (ready)
+app.get('/api/:id/:titulo', function(req, res){
+    db.open(function(error, mongoclient){
+        mongoclient.collection('postagens', function(error, collection){
+            collection.find({'_id': objectId(req.params.id), 'titulo': req.params.titulo}).toArray(function(error, result){
+                if(error){
+                    res.json(error);
+                }else{
+                    res.json(result);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+//PUT by Id (update)
+app.put('/api/:id', function(req, res){
+    db.open(function(error, mongoclient){
+        mongoclient.collection('postagens', function(error, collection){
+            collection.update(
+                {_id: objectId(req.params.id)},
+                {$set: {url_imagem: req.body.url_imagem}},
+                {},
+                function(error, result){
+                    if(error){
+                        res.json(error);
+                    }else{
+                        res.json(result);
+                    }
+
+                    mongoclient.close();
+
+                }
+            );
+        });
+    });
+});
+
+//DELETE by Id (delete)
+app.delete('/api/:id', function(req, res){
+    db.open(function(error, mongoclient){
+        mongoclient.collection('postagens', function(error, collection){
+            collection.remove(
+                {_id: objectId(req.params.id)},
+                function(error, result){
+                    if(error){
+                        res.json(error);
+                    }else{
+                        res.json(result);
+                    }
+
+                    mongoclient.close();
+                }
+            );
         });
     });
 });
